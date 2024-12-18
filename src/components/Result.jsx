c5873ec05891cb50743b52ea53abef733c3db16c
Diego Alvarez <diegoalvarezfrancos@gmail.com>
2024-12-18 13:50:21 +0100

    Primer commit

---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+
+const Result = ({value = "0"}) =>  (
+        <div className="result">
+            {value}
+        </div>
+    )
+
+Result.propTypes = {
+    value: PropTypes.string.isRequired
+}
+
+export default Result
---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+
+
+
+// Obtén el elemento raíz del DOM
+const rootElement = document.getElementById('root');
+
+// Crea una raíz en el contenedor
+const root = ReactDOM.createRoot(rootElement);
+
+// Renderiza el componente en la raíz creada
+root.render(<App />);
